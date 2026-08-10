0ceea977356805773823f909c6f569adbe16ab35
rocksun <daijun@gmail.com>
2026-08-10 10:19:22 +0800

    init

--- motrix_envs/src/motrix_envs/__init__.py
+++ motrix_envs/src/motrix_envs/__init__.py
@@ -13,4 +13,4 @@
 # limitations under the License.
 # ==============================================================================
 
-from . import basic, locomotion, manipulation  # noqa: F401
+from . import basic, locomotion, manipulation, navigation  # noqa: F401
